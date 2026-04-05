f86cdf48918f2714a26c548a03ef4f0032737836
Henry <wildfire101@qq.com>
2026-04-05 15:34:22 +0800

    data_model+modified

--- models.py
+++ models.py
@@ -13,7 +13,7 @@ valid_categories = [
 def create_transaction(date, amount, category, description, optional_notes=''):
     
     if category not in valid_categories:
-        print(f"Warning: Category '{category}' is not in the system's default list!"))
+        print(f"Warning: Category '{category}' is not in the system's default list!")
 
     transaction = {
         'date': date,
