8bc1ee904b6e1a5310a8623030c26d46b67b56c8
yuzhenxiang6-dotcom <yuzhenxiang6@gmail.com>
2026-04-21 12:12:52 +0800

    Update COMP1110-Budget-Assistant.py

--- COMP1110-Budget-Assistant.py
+++ COMP1110-Budget-Assistant.py
@@ -1,53 +1,48 @@
-valid_categories = ['Transport', 'Entertainment', 'Food', 'Shopping', 'Education', 'Sports', 'Utilities', 'Health', 'Others']
-
-def get_verified_input():
-    a=input('Please enter the amount: ')
-    while True:
-        try:
-            amount=float(a)
-            if amount<=0:
-                print('The amount must be bigger than 0, please try again: ')
-                continue
-            break
-        except ValueError:
-            print('invalid input, please try again:')
-
-def get_safe_category():
-    print("Available categories: ",', '.join(valid_categories))
-    while True:
-        category = input("Enter category: ")
-        if category in valid_categories:
-            return category
-        print('Invalid category. Please choose from',valid_categories)
+import file_IO
+import models
+import summary
 
 def main():
-    temp_database=[]
+    filename = 'text_transaction.json'
+    temp_database = file_IO.load_data(filename)
+
     while True:
-        print("💰 This is Personal Budget & Spending Assistant 💰")
+        print("\n💰 This is Personal Budget & Spending Assistant 💰")
         print("1. Add a new transaction")
-        print("2. View entered data (Test output)")
-        print("3. Exit system")
-        choice=input("please select an option(1-3): ")
-        if choice=='1':
+        print("2. View entered data")
+        print("3. View Summary Statistics")
+        print("4. Exit system")
+        choice = input("please select an option(1-4): ")
+        
+        if choice == '1':
             date = input('Please enter date (YYYY-MM-DD): ')
-            amount = get_verified_input()
-            category = get_safe_category()
+            amount = summary.get_verified_input() 
+            category = summary.get_safe_category()
             description = input('Please enter your description: ')
-            new_transaction = {'date': date,'amount': amount,'category': category,'description': description}
+            
+            new_transaction = models.create_transaction(date, amount, category, description)
             temp_database.append(new_transaction)
+            
+            file_IO.save_data(temp_database, filename)
             print("\nTransaction has been successfully validated and recorded!!!")
-        elif choice=='2':
+            
+        elif choice == '2':
             if not temp_database:
                 print("No transactions recorded yet.")
             else:
                 for item in temp_database:
                     print(item)
+                    
         elif choice == '3':
+            summary.generate_summary_statistics(temp_database)
+            
+        elif choice == '4':
+            file_IO.save_data(temp_database, filename)
             print("Data processing complete. Exiting system!")
             break
 
         else:
-            print("Invalid choice. Please enter 1, 2, or 3.")
+            print("Invalid choice. Please enter 1, 2, 3, or 4.")
 
 if __name__ == "__main__":
     main()
